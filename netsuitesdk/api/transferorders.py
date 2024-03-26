115bc01c6c3113c439b8bb7d5b640b16f3200d21
Dan Bunker <mcirque@gmail.com>
2024-03-26 07:43:08 -0400

    transfer orders

---
+++ netsuitesdk/api/transferorders.py
@@ -0,0 +1,88 @@
+from collections import OrderedDict
+
+from .base import ApiBase
+import logging
+from netsuitesdk.internal.exceptions import NetSuiteRequestError
+
+logger = logging.getLogger(__name__)
+
+class TransferOrders(ApiBase):
+    SIMPLE_FIELDS = [
+        'tranDate',
+        'itemList',
+    ]
+
+    RECORD_REF_FIELDS = [
+        'subsidiary',
+        'location',
+        'transferLocation',
+    ]
+
+    READ_ONLY_FIELDS = []
+
+    READ_ONLY_ITEM_FIELDS = [
+    ]
+
+    SIMPLE_ITEM_FIELDS = [
+        'quantityAvailable',
+        'quantityOnHand',
+        'quantityBackOrdered',
+        'quantityCommitted',
+        'quantityFulfilled',
+        'quantityPacked',
+        'quantityPicked',
+        'quantityReceived',
+        'quantity',
+    ]
+
+    RECORD_REF_ITEM_FIELDS = [
+        'item',
+    ]
+
+    def __init__(self, ns_client):
+        ApiBase.__init__(self, ns_client=ns_client, type_name='TransferOrder')
+
+    def get_by_internal_id(self, internalId):
+        return self.ns_client.get('TransferOrder', internalId=internalId)
+
+    def get_by_external_id(self, externalId):
+        return self.ns_client.get('TransferOrder', externalId=externalId)
+
+    def post(self, externalId, data) -> OrderedDict:
+        if externalId in ['', None, 0, [], {}]:
+            raise ValueError("externalId is required")
+        
+        transfer_order = self.ns_client.TransferOrder(externalId=externalId)
+
+        self.build_mb_simple_fields(self.SIMPLE_FIELDS, data, transfer_order)
+        self.build_mb_record_ref_fields(self.RECORD_REF_FIELDS, data, transfer_order)
+        self.build_mb_custom_fields(data, transfer_order)
+        self.remove_readonly(transfer_order, self.READ_ONLY_FIELDS)
+
+        if 'itemList' in data:
+            items = []
+            for item in data['itemList']['item']:
+                wsdl_item = self.ns_client.TransferOrderItem()
+                self.build_mb_simple_fields(self.SIMPLE_ITEM_FIELDS, item, wsdl_item)
+                self.build_mb_record_ref_fields(self.RECORD_REF_ITEM_FIELDS, item, wsdl_item)
+                self.build_mb_custom_fields(item, wsdl_item)
+                items.append(wsdl_item)
+
+            transfer_order.itemList = self.ns_client.TransferOrderItemList(item=items)
+
+        self.ns_client.upsert(transfer_order)
+        # try:
+        #     exists = True
+        #     self.ns_client.get('TransferOrder', externalId=externalId)
+        # except NetSuiteRequestError:
+        #     exists = False
+        #     self.logger.warning('TransferOrder not found for externalId {} - attempting to add it'.format(externalId))
+        #     # if sales order not found and the status is cancelled, just return and don't do anything
+        #     # if data['orderStatus'] == '_cancelled':
+        #     #     self.logger.warning('Cancelled SalesOrder not found - not adding externalId {}'.format(externalId))
+        #     #     return None
+
+        # if exists:
+        #     return self.ns_client.update(transfer_order)
+        # else:
+        #     return self.ns_client.add(transfer_order)
--- netsuitesdk/internal/netsuite_types.py
+++ netsuitesdk/internal/netsuite_types.py
@@ -165,6 +165,14 @@ COMPLEX_TYPES = {
         'File',
         'FileSearch'
     ],
+
+    # urn:inventory_2023_2.transactions.webservices.netsuite.com
+    # https://webservices.netsuite.com/xsd/transactions/v2023_2_0/inventory.xsd
+    'ns29': [
+        'TransferOrder',
+        'TransferOrderItem',
+        'TransferOrderItemList',
+    ],
 }
 
 SIMPLE_TYPES = {
@@ -179,3 +187,56 @@ SIMPLE_TYPES = {
         'SearchLongFieldOperator'
     ],
 }
+
+
+
+# NAMESPACES from NS with correct prefix
+# {
+#     'xsd': 'http://www.w3.org/2001/XMLSchema', 
+#     'ns0': 'urn:core_2023_2.platform.webservices.netsuite.com', 
+#     'ns1': 'urn:types.core_2023_2.platform.webservices.netsuite.com', 
+#     'ns2': 'urn:types.faults_2023_2.platform.webservices.netsuite.com', 
+#     'ns3': 'urn:faults_2023_2.platform.webservices.netsuite.com', 
+#     'ns4': 'urn:messages_2023_2.platform.webservices.netsuite.com', 
+#     'ns5': 'urn:common_2023_2.platform.webservices.netsuite.com', 
+#     'ns6': 'urn:types.common_2023_2.platform.webservices.netsuite.com', 
+#     'ns7': 'urn:scheduling_2023_2.activities.webservices.netsuite.com', 
+#     'ns8': 'urn:types.scheduling_2023_2.activities.webservices.netsuite.com', 
+#     'ns9': 'urn:communication_2023_2.general.webservices.netsuite.com', 
+#     'ns10': 'urn:types.communication_2023_2.general.webservices.netsuite.com', 
+#     'ns11': 'urn:filecabinet_2023_2.documents.webservices.netsuite.com', 
+#     'ns12': 'urn:types.filecabinet_2023_2.documents.webservices.netsuite.com', 
+#     'ns13': 'urn:relationships_2023_2.lists.webservices.netsuite.com', 
+#     'ns14': 'urn:types.relationships_2023_2.lists.webservices.netsuite.com', 
+#     'ns15': 'urn:support_2023_2.lists.webservices.netsuite.com', 
+#     'ns16': 'urn:types.support_2023_2.lists.webservices.netsuite.com', 
+#     'ns17': 'urn:accounting_2023_2.lists.webservices.netsuite.com', 
+#     'ns18': 'urn:types.accounting_2023_2.lists.webservices.netsuite.com', 
+#     'ns19': 'urn:sales_2023_2.transactions.webservices.netsuite.com', 
+#     'ns20': 'urn:types.sales_2023_2.transactions.webservices.netsuite.com', 
+#     'ns21': 'urn:purchases_2023_2.transactions.webservices.netsuite.com', 
+#     'ns22': 'urn:types.purchases_2023_2.transactions.webservices.netsuite.com', 
+#     'ns23': 'urn:customers_2023_2.transactions.webservices.netsuite.com', 
+#     'ns24': 'urn:types.customers_2023_2.transactions.webservices.netsuite.com', 
+#     'ns25': 'urn:financial_2023_2.transactions.webservices.netsuite.com', 
+#     'ns26': 'urn:types.financial_2023_2.transactions.webservices.netsuite.com', 
+#     'ns27': 'urn:bank_2023_2.transactions.webservices.netsuite.com',
+#     'ns28': 'urn:types.bank_2023_2.transactions.webservices.netsuite.com', 
+#     'ns29': 'urn:inventory_2023_2.transactions.webservices.netsuite.com', 
+#     'ns30': 'urn:types.inventory_2023_2.transactions.webservices.netsuite.com', 
+#     'ns31': 'urn:general_2023_2.transactions.webservices.netsuite.com', 
+#     'ns32': 'urn:customization_2023_2.setup.webservices.netsuite.com', 
+#     'ns33': 'urn:types.customization_2023_2.setup.webservices.netsuite.com', 
+#     'ns34': 'urn:employees_2023_2.lists.webservices.netsuite.com', 
+#     'ns35': 'urn:types.employees_2023_2.lists.webservices.netsuite.com', 
+#     'ns36': 'urn:website_2023_2.lists.webservices.netsuite.com', 
+#     'ns37': 'urn:types.website_2023_2.lists.webservices.netsuite.com', 
+#     'ns38': 'urn:employees_2023_2.transactions.webservices.netsuite.com', 
+#     'ns39': 'urn:types.employees_2023_2.transactions.webservices.netsuite.com', 
+#     'ns40': 'urn:marketing_2023_2.lists.webservices.netsuite.com', 
+#     'ns41': 'urn:types.marketing_2023_2.lists.webservices.netsuite.com', 
+#     'ns42': 'urn:demandplanning_2023_2.transactions.webservices.netsuite.com', 
+#     'ns43': 'urn:types.demandplanning_2023_2.transactions.webservices.netsuite.com', 
+#     'ns44': 'urn:supplychain_2023_2.lists.webservices.netsuite.com', 
+#     'ns45': 'urn:types.supplychain_2023_2.lists.webservices.netsuite.com'
+# }
